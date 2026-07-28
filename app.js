33ffc503478432fc53be394851b020603f97b6d9
NoirCreator <mbv6tvw5k4@privaterelay.appleid.com>
2026-07-28 14:42:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function openChat(name) {
     .getElementById("messages")
     .innerHTML = `
 
-        <div class="message incoming my-message">
+        <div class="message incoming">
 
             <div>
                 Привет! Добро пожаловать в NoirChat 🖤
