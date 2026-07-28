8e418e30eb557a0146af066b2609736930c30aa5
NoirCreator <mbv6tvw5k4@privaterelay.appleid.com>
2026-07-28 11:52:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,3 +43,36 @@ function sendMessage() {
     input.value = "";
 
 }
+function showScreen(screen) {
+
+    const chats = document.querySelector(".chats");
+    const profile = document.querySelector(".profile-screen");
+    const contacts = document.querySelector(".contacts-screen");
+
+
+    chats.style.display = "none";
+    profile.classList.add("hidden");
+    contacts.classList.add("hidden");
+
+
+    if(screen === "chats") {
+
+        chats.style.display = "block";
+
+    }
+
+
+    if(screen === "profile") {
+
+        profile.classList.remove("hidden");
+
+    }
+
+
+    if(screen === "contacts") {
+
+        contacts.classList.remove("hidden");
+
+    }
+
+}
